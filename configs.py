e0c6aaa1b1ff775559c525aa4b37ef72adfeabed
hyoniel2025-dot <hyoniel2025@gmail.com>
2026-03-25 15:32:38 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
 api_id = 29007359
 api_hash = "f9ac0af85457b9d39df7f7f871850bf2" #Esto se obtenien en mytelegramorg
-token = "8642461915:AAGkGFW9mboAnkvBvjSv-pxR78hcehHuLcY" #Crea token del bot en botfhater
+token = "8704378593:AAHQR4UDs14sHWEnw3gXO4lOfrDFobZRQN4" #Crea token del bot en botfhater
